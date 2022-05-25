c6f6a623864287d20f2f64c67de3c5e126c36852
Steve Morning <ocioej@gmail.com>
2022-05-25 15:30:49 -0300

    crear componente

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@
 import './styles.css';
 
 import{Todo , TodoList} from './classes'
+import { crearTodoHtml } from './js/componente';
 
 //import { Todo } from './classes/todo.class';
 //import { TodoList } from './classes/todo-list.class';
 
 
 const todoList = new TodoList();
-const tarea =  new Todo('Aprender JacaScript');
+const tarea =  new Todo('Aprender JacaScript!!!');
 const tarea2 =  new Todo('Comprar unicornio');
 
 
 todoList.nuevoTodo(tarea);
 todoList.nuevoTodo(tarea2);
 
+// tarea.completado = true;
 
 console.log(tarea);
 console.log(todoList);
+
+crearTodoHtml (tarea);
+crearTodoHtml (tarea2);
